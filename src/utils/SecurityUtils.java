3e429a6f7e251cb0d63f1d53a2ed2398f9966303
melodie2005 <melodieraveloarison@supinfo.com>
2026-01-29 23:44:29 +0400

    Initial commit

--- src/utils/SecurityUtils.java
+++ src/utils/SecurityUtils.java
@@ -7,9 +7,19 @@ public class SecurityUtils {
     public static String hashPassword(String password) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[]
+            byte[] encodeHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodeHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+
         } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erreur critique de securite", e);
         }
     }
 }
